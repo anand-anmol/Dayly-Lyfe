99db79b5e7ffea62cb5f46963dacf501a3014fe9
anand-anmol <anand.anmol150@gmail.com>
2020-05-01 09:47:24 -0700

    code review

--- app.js
+++ app.js
@@ -1,27 +1,38 @@
-const express = require('express')
-const fs = require('fs')
+var express = require('express');
 var http    = require('http');
 var path    = require('path');
 var engine  = require('ejs-locals');
 var app     = express();
 
+// Parse URL-encoded bodies (as sent by HTML forms)
+app.use(express.urlencoded({ extended: true }));
+
+// Parse JSON bodies (as sent by API clients)
+app.use(express.json());
+var cors = require('cors');
+app.use(cors());
+
 // Enable routing and use port 1337.
 require('./router')(app);
 app.set('port', 1337);
- 
- // Set up ejs templating.
+
+// Set up ejs templating.
 app.engine('ejs', engine);
 app.set('view engine', 'ejs');
- 
+
 // Set view folder.
 app.set('views', path.join(__dirname, 'views'));
- 
-// That line is to specify a directory where you could 
-// link to static files (images, CSS, etc.). 
-// So if you put a style.css file in that directory and you 
-// could link directly to it in your view <link href=”style.css” rel=”stylesheet”>
-app.use(express.static(path.join(__dirname, 'static')));
- 
+
+
+
+/* your regular r
+
+ */
+// Let's us reference images from a public image folder.
+app.use(express.static(__dirname+'/static'));
+
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
+
+module.exports = app;
---
+++ test/test.js
@@ -0,0 +1,16 @@
+// Import the dependencies for testing
+var chai     = require('chai')
+var chaiHttp = require('chai-http');
+var app      = require('../app.js');
+// var db       = require('../DailyLyfe');
+
+// Configure chai.
+// chai.use(chaiHttp);
+// chai.should();
+
+describe("Calendar", function() {
+    it('calendar shows correct month and year', function(done) {
+        console.log('momo')
+h               done();
+    })
+})
