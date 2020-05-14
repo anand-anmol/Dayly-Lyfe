9a5a8cd484f0548203618a30b2916f9745c163ab
Harish Anantharajah <harish7673@hotmail.com>
2020-05-14 13:51:55 -0700

    tests for routes

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ class TestAskMe(unittest.TestCase):
     
     def setUp(self):
         app.testing = True
-        self.app = app.test_client()
+        self.app = app.test_client(self)
 
     def tearDown(self):
         pass
@@ -13,6 +13,19 @@ class TestAskMe(unittest.TestCase):
     def test_page(self):
         response = self.app.get('/')
         self.assertEqual(response.status_code, 200)
+
+    def test_login(self):
+        response = self.app.get('/login', content_type='html/text')
+        self.assertEqual(response.status_code, 200)
+
+    def test_register(self):
+        response = self.app.get('/register')
+        self.assertEqual(response.status_code, 200)
+
+    def test_notes(self):
+        response = self.app.get('/notes')
+        self.assertEqual(response.status_code, 302)
+
 
 if __name__ == "__main__":
     unittest.main()
