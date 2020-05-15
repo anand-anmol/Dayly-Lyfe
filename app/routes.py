b5e9d6b98aea126b9305c9c81f395df814ba090f
Harish Anantharajah <harish7673@hotmail.com>
2020-05-15 12:52:33 -0700

    about

--- app/routes.py
+++ app/routes.py
@@ -12,6 +12,10 @@ import datetime
 def index():
     return render_template("index.html", title='Home Page')
 
+@app.route('/about')
+def about():
+    return render_template("about.html", title='About Page')
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
